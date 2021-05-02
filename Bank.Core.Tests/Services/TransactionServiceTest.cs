14ad9b976b4ddda2ecef220d11de206ec8db3d0c
Joakim Carlsson <joakimcarlsson1994@gmail.com>
2021-05-02 19:18:31 +0200

    Added unit test too check that we make a correct Withdraw.

--- Bank.Core.Tests/Services/TransactionServiceTest.cs
+++ Bank.Core.Tests/Services/TransactionServiceTest.cs
@@ -11,6 +11,7 @@ using Bank.Core.Repository.AccountRep;
 using Bank.Core.Repository.TranasctionsRep;
 using Bank.Core.Services.Transactions;
 using Bank.Core.ViewModels.Transactions;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 
@@ -42,7 +43,7 @@ namespace Bank.Core.Tests.Services
             _account = new Account
             {
                 AccountId = 5,
-                Balance = 1000,
+                Balance = 1500,
             };
         }
 
@@ -64,7 +65,7 @@ namespace Bank.Core.Tests.Services
 
             var model = new DepositViewModel
             {
-                AccountId = 5,
+                AccountId = _account.AccountId,
                 Amount = 500
             };
 
@@ -74,7 +75,7 @@ namespace Bank.Core.Tests.Services
 
             await sut.SaveDepositAsync(model).ConfigureAwait(false);
 
-            //assert
+            //Assert
             _transactionRepository.Verify(x => x.AddAsync(It.IsAny<Transaction>()), Times.Once());
             _accountRepository.Verify(x => x.GetByIdAsync(_account.AccountId), Times.Once());
             _accountRepository.Verify(x => x.UpdateAsync(_account), Times.Once());
@@ -84,6 +85,45 @@ namespace Bank.Core.Tests.Services
             Assert.AreEqual("Credit", savedTransaction.Type);
             Assert.AreEqual(DateTime.Now.ToString(), savedTransaction.Date.ToString());
             Assert.AreEqual(model.Amount, savedTransaction.Amount);
+            Assert.AreEqual(model.AccountId, savedTransaction.AccountId);
+            Assert.AreEqual(_account.Balance, savedTransaction.Balance);
+        }
+
+        [TestMethod]
+        public async Task ShouldSaveWithdrawTransaction()
+        {
+            //Arrange
+            Transaction savedTransaction = null;
+            _transactionRepository.Setup(x => x.AddAsync(It.IsAny<Transaction>())).Callback<Transaction>(transaction =>
+            {
+                savedTransaction = transaction;
+            });
+
+            _accountRepository.Setup(x => x.GetByIdAsync(_account.AccountId)).ReturnsAsync(_account);
+            _accountRepository.Setup(x => x.UpdateAsync(_account));
+
+            var configuration = new MapperConfiguration(cfg => cfg.CreateMap<WithdrawViewModel, Transaction>().IgnoreAllNonExisting());
+            configuration.AssertConfigurationIsValid();
+
+            var model = new WithdrawViewModel
+            {
+                AccountId = _account.AccountId,
+                Amount = 500,
+            };
+
+            //Act
+            var sut = new TransactionService(_mapper, _transactionRepository.Object, _accountRepository.Object);
+            await sut.SaveWithdrawAsync(model).ConfigureAwait(false);
+
+            //Assert
+            _transactionRepository.Verify(x => x.AddAsync(It.IsAny<Transaction>()), Times.Once());
+            _accountRepository.Verify(x => x.GetByIdAsync(_account.AccountId), Times.Once());
+            _accountRepository.Verify(x => x.UpdateAsync(_account), Times.Once());
+            Assert.IsNotNull(savedTransaction);
+            Assert.AreEqual("Withdraw", savedTransaction.Operation);
+            Assert.AreEqual("Credit", savedTransaction.Type);
+            Assert.AreEqual(DateTime.Now.ToString(), savedTransaction.Date.ToString());
+            Assert.AreEqual(-model.Amount, savedTransaction.Amount);
             Assert.AreEqual(model.AccountId, savedTransaction.AccountId);
             Assert.AreEqual(_account.Balance, savedTransaction.Balance);
         }
