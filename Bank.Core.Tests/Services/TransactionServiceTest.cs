a113f8165593254b91e7546aea1e4e43dc27d434
Joakim Carlsson <joakimcarlsson1994@gmail.com>
2021-05-02 19:37:56 +0200

    Added test so we make sure we make valid transfer transactions.

--- Bank.Core.Tests/Services/TransactionServiceTest.cs
+++ Bank.Core.Tests/Services/TransactionServiceTest.cs
@@ -47,8 +47,10 @@ namespace Bank.Core.Tests.Services
             };
         }
 
-        [TestMethod]
-        public async Task ShouldSaveDepositTransaction()
+        [DataTestMethod]
+        [DataRow(5000)]
+        [DataRow(4635)]
+        public async Task ShouldSaveDepositTransaction(int amount)
         {
             //Arrange
             Transaction savedTransaction = null;
@@ -66,7 +68,7 @@ namespace Bank.Core.Tests.Services
             var model = new DepositViewModel
             {
                 AccountId = _account.AccountId,
-                Amount = 500
+                Amount = amount
             };
 
 
@@ -89,8 +91,10 @@ namespace Bank.Core.Tests.Services
             Assert.AreEqual(_account.Balance, savedTransaction.Balance);
         }
 
-        [TestMethod]
-        public async Task ShouldSaveWithdrawTransaction()
+        [DataTestMethod]
+        [DataRow(400)]
+        [DataRow(635)]
+        public async Task ShouldSaveWithdrawTransaction(int amount)
         {
             //Arrange
             Transaction savedTransaction = null;
@@ -108,7 +112,7 @@ namespace Bank.Core.Tests.Services
             var model = new WithdrawViewModel
             {
                 AccountId = _account.AccountId,
-                Amount = 500,
+                Amount = amount,
             };
 
             //Act
@@ -126,6 +130,64 @@ namespace Bank.Core.Tests.Services
             Assert.AreEqual(-model.Amount, savedTransaction.Amount);
             Assert.AreEqual(model.AccountId, savedTransaction.AccountId);
             Assert.AreEqual(_account.Balance, savedTransaction.Balance);
+        }
+
+        [DataTestMethod]
+        [DataRow(400)]
+        [DataRow(600)]
+        public async Task ShouldSaveTransferTransactions(int amount)
+        {
+            //Arrange
+            var toAccount = new Account
+            {
+                AccountId = 1,
+                Balance = 1000
+            };
+
+            List<Transaction> transactions = new List<Transaction>();
+            _transactionRepository.Setup(x => x.AddAsync(It.IsAny<Transaction>())).Callback<Transaction>(transaction =>
+            {
+                transactions.Add(transaction);
+            });
+
+            _accountRepository.Setup(x => x.GetByIdAsync(_account.AccountId)).ReturnsAsync(_account);
+            _accountRepository.Setup(x => x.GetByIdAsync(toAccount.AccountId)).ReturnsAsync(toAccount);
+            _accountRepository.Setup(x => x.UpdateAsync(_account));
+            _accountRepository.Setup(x => x.UpdateAsync(toAccount));
+
+            var configuration = new MapperConfiguration(cfg => cfg.CreateMap<WithdrawViewModel, Transaction>().IgnoreAllNonExisting());
+            configuration.AssertConfigurationIsValid();
+
+            var model = new TransferViewModel()
+            {
+                FromAccountId = _account.AccountId,
+                ToAccountId = toAccount.AccountId,
+                Amount = amount,
+            };
+
+            //Act
+            var sut = new TransactionService(_mapper, _transactionRepository.Object, _accountRepository.Object);
+            await sut.SaveTransferAsync(model).ConfigureAwait(false);
+
+            //Assert
+            _transactionRepository.Verify(x => x.AddAsync(It.IsAny<Transaction>()), Times.Exactly(2));
+            _accountRepository.Verify(x => x.GetByIdAsync(It.IsAny<int>()), Times.Exactly(2));
+            _accountRepository.Verify(x => x.UpdateAsync(It.IsAny<Account>()), Times.Exactly(2));
+
+            Assert.AreEqual(transactions.Count, 2);
+            Assert.AreEqual("Transfer to another account.", transactions[0].Operation);
+            Assert.AreEqual("Credit", transactions[0].Type);
+            Assert.AreEqual(DateTime.Now.ToString(), transactions[0].Date.ToString());
+            Assert.AreEqual(-model.Amount, transactions[0].Amount);
+            Assert.AreEqual(model.FromAccountId, transactions[0].AccountId);
+            Assert.AreEqual(_account.Balance, transactions[0].Balance);
+
+            Assert.AreEqual("Transfer from another account.", transactions[1].Operation);
+            Assert.AreEqual("Credit", transactions[1].Type);
+            Assert.AreEqual(DateTime.Now.ToString(), transactions[1].Date.ToString());
+            Assert.AreEqual(model.Amount, transactions[1].Amount);
+            Assert.AreEqual(model.ToAccountId, transactions[1].AccountId);
+            Assert.AreEqual(toAccount.Balance, transactions[1].Balance);
         }
     }
 }
