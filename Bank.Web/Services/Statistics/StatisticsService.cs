4927d85ae8d54f9d6253f76b77bfb188ff09bbf0
Joakim Carlsson <joakimcarlsson1994@gmail.com>
2021-05-19 21:26:00 +0200

    Obs I did it again.

--- Bank.Web/Services/Statistics/StatisticsService.cs
+++ Bank.Web/Services/Statistics/StatisticsService.cs
@@ -38,44 +38,27 @@ namespace Bank.Web.Services.Statistics
             };
         }
 
-        //TODO FIX ME
         public async Task<CountryStatisticsViewModel> GetCountryStatisticsAsync()
         {
-            var model = new CountryStatisticsViewModel {Countries = new List<Item>()};
-            
-            var customers = (from customer in _dbContext.Customers
-                let accounts = _dbContext.Dispositions.Include(a => a.Account)
-                    .Where(i => i.CustomerId == customer.CustomerId && i.Type == "OWNER")
-                    .Select(i => i.Account)
-                    .ToList()
-                where accounts.Count != 0
-                select new {Customer = customer, Accounts = accounts,}).ToList();
-
-            var groupedCountries = customers.GroupBy(c => c.Customer.Country);
-
-            foreach (var grouping in groupedCountries)
+            var customersQuery = await _customerRepository.ListAllAsync();
+            return new CountryStatisticsViewModel
             {
-                int customersAmount = 0;
-                int accountsAmount = 0;
-                decimal accountsTotalBalance = 0;
-
-                foreach (var test in grouping)
-                {
-                    customersAmount += 1;
-                    accountsAmount += test.Accounts.Count;
-                    accountsTotalBalance += test.Accounts.Select(i => i.Balance).Sum();
-                }
-
-                model.Countries.Add(new Item
-                {
-                    Country = grouping.Key, 
-                    CustomerAmount = customersAmount,
-                    AccountAmount = accountsAmount,
-                    AccountsTotalBalance = accountsTotalBalance
-                });
-            }
-
-            return model;
+                Countries = (from customer in customersQuery
+                        let accounts = _dbContext.Dispositions.Include(a => a.Account)
+                            .Where(i => i.CustomerId == customer.CustomerId && i.Type == "OWNER")
+                            .Select(i => i.Account)
+                            .ToList()
+                        where accounts.Count != 0
+                        select new {Customer = customer, Accounts = accounts, TotalSum = accounts.Sum( i => i.Balance)})
+                    .AsEnumerable().GroupBy(c => new {c.Customer.Country})
+                    .Select(t => new Item()
+                    {
+                        Country = t.Key.Country,
+                        CustomerAmount = t.Count(),
+                        AccountAmount = t.Sum(c => c.Accounts.Count),
+                        AccountsTotalBalance = t.Sum(i => i.TotalSum)
+                    }).ToList()
+            };
         }
 
         public async Task<IEnumerable<TopCustomerViewModel>> GetTopCustomersByCountry(int amount, string country)
